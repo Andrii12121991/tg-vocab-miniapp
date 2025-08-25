0a9980ea9d081d98a8263599aaae022a9c008dcb
Andrii12121991 <115224593+Andrii12121991@users.noreply.github.com>
2025-08-25 13:16:11 +0700

    Delete app directory

--- app/lib/telegram.js
+++
@@ -1,9 +0,0 @@
-
-export function getUserId(req) {
-  const authHeader = req.headers.get("authorization");
-  if (!authHeader) throw new Error("No auth header");
-
-  const [_, token] = authHeader.split(" ");
-  const user = JSON.parse(atob(token.split(".")[1]));
-  return user.id;
-}
